8cd625f40e3a794c3e954b9523e6c24ae8d979e6
carlagamez <carlagamez95@gmail.com>
2025-04-19 20:32:44 +0200

    Añadir script de consultas SQL para venta y alquiler

--- consultas.py
+++ consultas.py
@@ -1,17 +1,35 @@
 import sqlite3
 import pandas as pd
-conn = sqlite3.connect("venta_madrid.db")
-print('\npisos con más de 3 habitaciones:')
-df = pd.read_sql_query("SELECT * FROM venta_madrid WHERE habitaciones > 3", conn)
-print(df)
-print('\npisos con más de 90 metros:')
-df = pd.read_sql_query("SELECT * FROM venta_madrid WHERE metros > 90", conn)
-print(df)
-print('\npisos con más de 200000 euros:')
-df = pd.read_sql_query("SELECT * FROM venta_madrid WHERE precio > 200000", conn)
-print(df)
-conn.close()
-# Cerrar la conexión
-# print("La conexión a la base de datos ha sido cerrada.")
-# # La conexión a la base de datos ha sido cerrada.
-# print("El archivo 'venta_madrid_modelado.csv' ha sido creado con éxito.")
+
+# ----------- VENTAS -----------
+print("[VENTA] Conectando con la base de datos...")
+conn_venta = sqlite3.connect("venta_madrid.db")
+
+print("\n[VENTA] Pisos con más de 3 habitaciones:")
+df_venta_hab = pd.read_sql_query(
+    "SELECT * FROM venta_madrid WHERE habitaciones > 3", conn_venta)
+print(df_venta_hab)
+
+print("\n[VENTA] Pisos con más de 90 metros:")
+df_venta_metros = pd.read_sql_query(
+    "SELECT * FROM venta_madrid WHERE metros > 90", conn_venta)
+print(df_venta_metros)
+
+print("\n[VENTA] Pisos con más de 200000 euros:")
+df_venta_precio = pd.read_sql_query(
+    "SELECT * FROM venta_madrid WHERE precio > 200000", conn_venta)
+print(df_venta_precio)
+
+conn_venta.close()
+
+
+# ----------- ALQUILER -----------
+print("\n[ALQUILER] Consultando base de datos...")
+conn_alquiler = sqlite3.connect("alquiler_madrid.db")
+
+print("\n[ALQUILER] Inmuebles con más de 90m² construidos:")
+df_alq_superficie = pd.read_sql_query(
+    "SELECT * FROM alquiler_madrid WHERE Superficie_construida > 90", conn_alquiler)
+print(df_alq_superficie)
+
+conn_alquiler.close()
